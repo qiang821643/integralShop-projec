ea2beb3b99788e87d2de2bffb515eb6f61ba4046
QiangShW <821643687@qq.com>
2019-06-20 16:23:27 +0800

    更改fastDFS地址 从代码换到配置文件

--- src/main/java/com/integral/util/FileUtils.java
+++ src/main/java/com/integral/util/FileUtils.java
@@ -6,6 +6,7 @@ import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,34 +17,38 @@ import java.io.IOException;
 public class FileUtils {
 
     @Autowired
-    private  FastFileStorageClient storageClient;
+    private FastFileStorageClient storageClient;
 
+    @Value("${file-url}")
+    private String fileUrl;
 
     /**
      * 上传文件
+     *
      * @param file
      * @return
      * @throws IOException
      */
     public String upload(MultipartFile file) throws IOException {
 
-        StorePath storePath = storageClient.uploadFile(file.getInputStream(),file.getSize(),
-                FilenameUtils.getExtension(file.getOriginalFilename()),null);
-        StringBuffer path= new StringBuffer("http://47.94.251.90:8502/").append(storePath.getFullPath());
+        StorePath storePath = storageClient.uploadFile(file.getInputStream(), file.getSize(),
+                FilenameUtils.getExtension(file.getOriginalFilename()), null);
+        StringBuffer path = new StringBuffer(fileUrl).append(storePath.getFullPath());
         return path.toString();
     }
 
     /**
      * 删除文件
+     *
      * @param fileUrl
      */
-    public  void deleteFile(String fileUrl){
+    public void deleteFile(String fileUrl) {
 
-        if(!StringUtils.isNotBlank(fileUrl)){
+        if (!StringUtils.isNotBlank(fileUrl)) {
             return;
         }
         StorePath storePath = StorePath.praseFromUrl(fileUrl);
-        storageClient.deleteFile(storePath.getGroup(),storePath.getPath());
+        storageClient.deleteFile(storePath.getGroup(), storePath.getPath());
 
     }
 
